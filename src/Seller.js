7e0f4ceb0f471c9e14a8ea681eac0448632ca0ed
NouresSaleh <Neu991127>
2022-10-12 21:53:47 +1100

    Initial commit

--- src/Seller.js
+++ src/Seller.js
@@ -1,5 +1,5 @@
 import {Link} from "react-router-dom";
-import { useLocation, useNavigate, Navigate  } from 'react-router-dom';
+import { useLocation, useNavigate, navigate  } from 'react-router-dom';
 import {useState} from "react";
 
 const Seller = () => {
